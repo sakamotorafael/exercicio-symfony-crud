11362b3a72c6ef09fd4ba8f59ffbd63d5918d62d
root <rafael.sakamoto@gmail.com>
2021-04-29 15:39:59 -0300

    more fixes

--- src/src/Service/InfoStringService.php
+++ src/src/Service/InfoStringService.php
@@ -3,9 +3,13 @@
 namespace App\Service;
 
 use App\Entity\Oeuvre;
+use App\Entity\Piece;
 
 class InfoStringService {
   public function oeuvre(Oeuvre $oeuvre){
     return $oeuvre->getName() . " by " . $oeuvre->getComposer()->getName(); 
+  }
+  public function piece(Piece $piece){
+    return $piece->getTitle() . " n. " . $piece->getNumber() . " in " . $piece->getTonality();  
   }
 }
